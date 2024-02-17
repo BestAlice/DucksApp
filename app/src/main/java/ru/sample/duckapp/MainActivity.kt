87dcc45ce4d934c3386e89c7f5a708bd6e58fcfb
Dima <dima_goloskok@lenta.ru>
2024-02-17 14:17:19 +0300

    button and image in center

--- app/src/main/java/ru/sample/duckapp/MainActivity.kt
+++ app/src/main/java/ru/sample/duckapp/MainActivity.kt
@@ -2,10 +2,20 @@ package ru.sample.duckapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var duckView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        duckView = findViewById(R.id.DuckView)
+
+        Picasso.get().load(R.drawable.placeholder).placeholder(R.drawable.placeholder)
+            .error(R.drawable.placeholder)
+            .into(duckView)
     }
 }
