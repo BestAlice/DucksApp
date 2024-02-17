fa09ea764c83b974c4cf162349aa82d8a905832c
Dima <dima_goloskok@lenta.ru>
2024-02-17 15:26:59 +0300

    Succesfull Duck getting

--- app/src/main/java/ru/sample/duckapp/MainActivity.kt
+++ app/src/main/java/ru/sample/duckapp/MainActivity.kt
@@ -2,8 +2,14 @@ package ru.sample.duckapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import com.squareup.picasso.Picasso
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import ru.sample.duckapp.domain.Duck
+import ru.sample.duckapp.infra.Api
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,8 +20,42 @@ class MainActivity : AppCompatActivity() {
 
         duckView = findViewById(R.id.DuckView)
 
-        Picasso.get().load(R.drawable.placeholder).placeholder(R.drawable.placeholder)
-            .error(R.drawable.placeholder)
+        findViewById<Button>(R.id.button)
+            .setOnClickListener {
+                makeRequest()
+            }
+
+
+
+
+        //val call: Call<Duck> =
+
+
+
+    }
+
+    fun makeRequest(){
+        Api.ducksApi.getRandomDuck().enqueue(object : Callback<Duck> {
+
+            override fun onResponse(call: Call<Duck>, response: Response<Duck>) {
+                if (response.isSuccessful) {
+                    val duck: Duck? = response.body()
+
+                    duck?.url?.let { setDuckImage(it) };
+                } else {
+                    println("Request failed with code: ${response.code()}")
+                }
+            }
+
+            override fun onFailure(call: Call<Duck>, t: Throwable) {
+                println("Request failed: ${t.message}")
+            }
+        })
+    }
+
+    fun setDuckImage(url: String){
+        Picasso.get().load(url).placeholder(R.drawable.placeholder)
+            .error(R.drawable.owl)
             .into(duckView)
     }
 }
